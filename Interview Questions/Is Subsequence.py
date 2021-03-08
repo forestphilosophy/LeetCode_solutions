6baec3ef835799014d65a553f75b636f5237d80a
Jimmy Z. Lin <43326009+forestphilosophy@users.noreply.github.com>
2021-03-08 11:11:26 +0100

    Create Is Subsequence.py

---
+++ Interview Questions/Is Subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        if len(s) == 0:
+            return True 
+        
+        s_pointer = 0
+        t_pointer = 0
+        
+        while  t_pointer < len(t):
+            
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+                if s_pointer == len(s):
+                    return True
+   
+            t_pointer += 1
+        
+        return False
