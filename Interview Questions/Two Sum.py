e0250fd1565d39cb95a01c3c706fa105a19d8344
Jimmy Z. Lin <43326009+forestphilosophy@users.noreply.github.com>
2022-10-10 15:08:24 +0200

    Update Two Sum.py

--- Interview Questions/Two Sum.py
+++ Interview Questions/Two Sum.py
@@ -1,10 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            if i == len(nums):
-                continue
-            else:
-                remaining_arr = nums[i+1:]
-                for j in range(len(remaining_arr)):
-                    if nums[i] + remaining_arr[j] == target:
-                        return [i,i+j+1]
+        pointer_1 = 0
+        pointer_2 = len(nums) - 1
+        
+        heap_map = [[idx,val] for idx,val in enumerate(nums)]
+        heap_map.sort(key = lambda x: x[1])
+
+        while pointer_1 < pointer_2:
+            
+            num1 = heap_map[pointer_1][1]
+            num2 = heap_map[pointer_2][1]
+            
+            if num1 + num2 == target:
+                return [heap_map[pointer_1][0], heap_map[pointer_2][0]]
+            
+            elif num1 + num2 > target:
+                pointer_2 -= 1
+                
+            elif num1 + num2 < target:
+                pointer_1 += 1
+                
+                
+        return []
+        
