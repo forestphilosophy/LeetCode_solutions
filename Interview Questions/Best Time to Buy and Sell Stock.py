c131e0f45613e6c742031c69214ce9f1d64de237
Jimmy Z. Lin <43326009+forestphilosophy@users.noreply.github.com>
2023-02-01 11:26:10 +0800

    Update Best Time to Buy and Sell Stock.py

--- Interview Questions/Best Time to Buy and Sell Stock.py
+++ Interview Questions/Best Time to Buy and Sell Stock.py
@@ -1,29 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit_list = []
+        profit = 0
+        max_profit = 0
+
+        minimum = prices[0]
         
-        if len(prices) == 1:
-            return 0
-        
-        for i in range(0,len(prices)):
-            if i == len(prices)-1:
-                continue
-                
-            if prices[i] > max(prices[i+1:]):
-                profit_list.append(0)
-                continue
-            else:
-                profit_list.append(max(prices[i+1:])-prices[i])
-                
-        return max(profit_list)
-    
-#Efficient solution
-def maxProfit(self, prices: List[int]) -> int:
-        largest_profit = 0
-        min_purchase = prices[0]
-        for i in range(1, len(prices)):
-            if prices[i] - min_purchase > largest_profit:
-                largest_profit = prices[i] - min_purchase
-            if prices[i] < min_purchase:
-                min_purchase = prices[i]
-        return largest_profit
+        for i in range(len(prices)):
+            minimum = min(minimum,prices[i])
+            profit = prices[i] - minimum
+            max_profit = max(profit,max_profit)
+
+        return max_profit
