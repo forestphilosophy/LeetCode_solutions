1ab0d6018ea205bc9a3538643ec1ba2237250efe
Jimmy Z. Lin <43326009+forestphilosophy@users.noreply.github.com>
2023-03-25 12:19:50 +0800

    Create set_matrix_zeros.py

---
+++ Interview Questions/set_matrix_zeros.py
@@ -0,0 +1,26 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        m,n = len(matrix),len(matrix[0])
+        Visited = set()
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0 and (i,j) not in Visited:
+                    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                        x = i
+                        y = j
+
+                        while True:
+                            x += dx
+                            y += dy
+
+                            if x < 0 or x >= m or y < 0 or y >= n:
+                                break
+                                
+                            if matrix[x][y] != 0:
+                                Visited.add((x,y))
+                                matrix[x][y] = 0
